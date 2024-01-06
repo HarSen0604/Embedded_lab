91e2857677cf8c93e1643915925499cd3fe4a497
Hareesh S <105427651+HarSen0604@users.noreply.github.com>
2024-01-06 13:13:32 +0530

    Expt2

---
+++ Expt2/Code/Exercise/exercise2.ino
@@ -0,0 +1,29 @@
+#define RED 11
+#define GREEN 10
+#define BLUE 9
+
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+
+  delay(1000);
+
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, LOW);
+
+  delay(1000);
+
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, HIGH);
+
+  delay(1000);
+}
