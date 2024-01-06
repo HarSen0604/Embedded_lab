8f526a4fbde6f5a5e21ab6cc32b67c37167beb9e
Hareesh S <105427651+HarSen0604@users.noreply.github.com>
2024-01-06 12:46:51 +0530

    Blink the in - built LED

---
+++ Expt1/blinkingLED.ino
@@ -0,0 +1,14 @@
+// Blink the in - built LED of the Arduino UNO board
+#define LED 13
+
+void setup() {
+  pinMode(LED, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+}
