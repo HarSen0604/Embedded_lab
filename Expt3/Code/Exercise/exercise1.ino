d43924e8c1402ac196fc5eb1df84bee5f8d1a399
Hareesh S <105427651+HarSen0604@users.noreply.github.com>
2024-01-22 14:22:31 +0530

    Update and rename temp.txt to exercise1.ino

---
+++ Expt3/Code/Exercise/exercise1.ino
@@ -0,0 +1,28 @@
+#include <Keypad.h>
+
+#define ROW_NUM    4
+#define COLUMN_NUM 3
+
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+byte pin_rows[ROW_NUM] = {9, 8, 7, 6};
+byte pin_column[COLUMN_NUM] = {5, 4, 3};
+
+Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char key = keypad.getKey();
+  
+  if (key) {
+    Serial.println("Key Pressed: " + String(key));
+  }
+}
