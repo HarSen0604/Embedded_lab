9ffab5b5512d3f4a206c222718615ed19bcd42ec
Hareesh S <105427651+HarSen0604@users.noreply.github.com>
2024-02-04 21:48:50 +0530

    Expt3

--- Expt3/Code/Exercise/exercise2.ino
+++ Expt3/Code/Exercise/exercise2.ino
@@ -10,8 +10,8 @@ char keys[ROW_NUM][COLUMN_NUM] = {
   {'*','0','#'}
 };
 
-byte pin_rows[ROW_NUM] = {9, 8, 7, 6};
-byte pin_column[COLUMN_NUM] = {5, 4, 3};
+byte pin_rows[ROW_NUM] = {10, 9, 8, 7};
+byte pin_column[COLUMN_NUM] = {6, 5, 4};
 
 void setup() {
   Serial.begin(9600);
