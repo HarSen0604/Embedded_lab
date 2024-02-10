db1fef06c92ed62bf1d37dfa1bc0cd99da210c2c
Hareesh S <105427651+HarSen0604@users.noreply.github.com>
2024-02-10 12:27:56 +0530

    Update postlab1.ino

--- Expt4/Code/Post lab/postlab1.ino
+++ Expt4/Code/Post lab/postlab1.ino
@@ -1,1 +1,6 @@
+/*
+Get the input from keypad (password) and display it on LCD Screen. 
+Let the pin be 1234. Get the input pin from the user. 
+If it matches the given pin display welcome on the LCD screen. Else display Invalid user
+*/
 
