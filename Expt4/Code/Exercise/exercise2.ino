31ab21a870368d8c6e3b6bc662f3d13ee624b5a4
Hareesh S <105427651+HarSen0604@users.noreply.github.com>
2024-02-10 12:20:57 +0530

    Expt4

---
+++ Expt4/Code/Exercise/exercise2.ino
@@ -0,0 +1,21 @@
+/*
+Scroll the given text. (19Z604 – Embedded Systems Lab)
+*/
+
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("19z604 Embedded Systems Laboratory");
+  delay(1000);
+}
+
+void loop() {
+  lcd.setCursor(0, 0);
+  for (int positionCounter = 0; positionCounter < 16; positionCounter++) {
+    lcd.scrollDisplayLeft();
+    delay(100);
+  }
+}
